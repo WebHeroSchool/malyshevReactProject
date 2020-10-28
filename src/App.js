f83b7309dfaf49fda1dcc9c0d4790236410672a8
Maxim Malyshev <malyshev21@gmail.com>
2020-10-28 20:42:19 +0300

    React-element

--- src/App.js
+++ src/App.js
@@ -1,36 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import * as nums from './numbers';
+const element = (<div>
+  Hello Mentor! Have a nice day!
+  </div>);
 
-const result = 'count * length = ' + nums.count * nums.length;
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style={{
-          color: 'blue'
-        }}>
-        Hello Mentor
-        </p>
-        <p>
-        {result}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
--- src/numbers.js
+++
@@ -1,3 +0,0 @@
-const count = 17;
-const length = 3;
-export { count, length };
