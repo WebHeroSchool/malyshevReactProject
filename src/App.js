83597138d155d7c3f66738c457aacdd60a336975
Maxim Malyshev <malyshev21@gmail.com>
2020-10-29 19:51:16 +0300

    React-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-const element = (<div>
-  Hello Mentor! Have a nice day!
+const ItemList = () => (<ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+</ul>);
+const App = () => (<div>
+  <h1>Hello Mentor!</h1>
+  <p>Have a nice day!</p>
+  <ItemList />
   </div>);
 
-export { element };
+export default App;
