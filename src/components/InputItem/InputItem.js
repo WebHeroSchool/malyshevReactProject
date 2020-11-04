9572307af564a5e82ff07be102e4b147789827fb
Maxim Malyshev <malyshev21@gmail.com>
2020-11-04 17:19:55 +0300

    Material UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,3 +1,17 @@
-const InputItem = () => (<input placeholder="What needs to be done?"/>)
+import TextField from '@material-ui/core/TextField';
+
+const InputItem = () => (<div>
+  <TextField
+  id="outlined-full-width"
+  style={{ margin: 8 }}
+  placeholder="What needs to be done?"
+  fullWidth
+  margin="normal"
+  InputLabelProps={{
+    shrink: true,
+  }}
+  variant="outlined"
+   />
+   </div>)
 
 export default InputItem;
