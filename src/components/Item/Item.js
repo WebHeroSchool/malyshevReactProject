2da29318d09f7c2b441e1ec6f2e5210072d29e53
Maxim Malyshev <malyshev21@gmail.com>
2020-11-10 14:31:23 +0300

    Event handling

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import classnames from 'classnames';
-import styles from './Item.module.css'
+import styles from './Item.module.css';
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
-const Item = ({ value, isDone }) => (<span className={
+const Item = ({ value, isDone, onClickDone }) => (<button
+  onClick={() => console.log(isDone)}
+  className={
   classnames({
     [styles.item]: true,
     [styles.done]: isDone
   })
 }>
 {value}
-</span>);
+</button>);
 
 export default Item;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,11 +1,10 @@
 import Item from '../Item/Item';
-import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-const ItemList = ( {items}) => (<ul>
+const ItemList = ( { items, onClickDone }) => (<ul>
   {items.map(item => <li key={item.value}>
-    <Item value={item.value} isDone={item.isDone} />
+    <Item value={item.value} isDone={item.isDone} onClickDone={onClickDone} />
     <IconButton aria-label="delete">
     <DeleteIcon fontSize="small" />
     </IconButton>
